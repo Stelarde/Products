cb291b950ad037f5f7afbe4881b7f8d0de29f8c7
Stelarde <maksimyudin31@gmail.com>
2021-07-03 20:02:25 +0300

    update

--- view/viewTable.php
+++ view/viewTable.php
@@ -1,15 +1,15 @@
 <?php 
 $products = require (__DIR__ . '/../controller/controlTable.php');    
 ?>
-<table class="table" id="productTable">
+<table id="productTable">
     <thead>
         <tr>
-          <th scope="col">#</th>
-          <th scope="col">Название</th>
-          <th scope="col">Цена</th>
-          <th scope="col">Артикул</th>
-          <th scope="col">Количество</th>
-          <th scope="col">Дата появления записи</th>
+          <th>#</th>
+          <th>Название</th>
+          <th>Цена</th>
+          <th>Артикул</th>
+          <th>Количество</th>
+          <th>Дата появления записи</th>
       </tr>
   </thead>
   <?php
@@ -17,18 +17,18 @@ $products = require (__DIR__ . '/../controller/controlTable.php');
     ?>
     <tbody>
         <tr>
-          <th scope="row"><?php echo $value['PRODUCT_ID'] ?></th>
+          <th><?php echo $value['PRODUCT_ID'] ?></th>
           <td><?php echo $value['PRODUCT_NAME'] ?></td>
           <td><?php echo $value['PRODUCT_PRICE'] ?></td>
           <td><?php echo $value['PRODUCT_ARTICLE'] ?></td>
           <td>
-            <button type="button" class="btn btn-secondary btn-sm" id="less">-</button>
+            <button type="button" id="less">-</button>
             <label id="labelQuantity"><?php echo $value['PRODUCT_QUANTITY'] ?></label>            
-            <button type="button" class="btn btn-secondary btn-sm" id="plus">+</button>        
+            <button type="button" id="plus">+</button>        
           </td>
           <td><?php echo $value['DATE_CREATE'] ?></td>
           <td>
-            <button type="button" class="btn btn-secondary" id="hide">Скрыть</button>
+            <button type="button" id="hide">Скрыть</button>
         </td>
     </tr>
     <?php
